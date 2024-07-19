a0b88f6ff12167b28bb6e83b3f313110c6633499
y0dev <devdoesit17@gmail.com>
2024-07-19 05:37:44 -0500

    added embedded example

---
+++ windows/embedded_ar_example/sources/arithmetic.c
@@ -0,0 +1,26 @@
+#include "arithmetic.h"
+#include "debug/debug,h"
+
+int add(int a, int b) 
+{
+    return a + b;
+}
+
+int subtract(int a, int b) 
+{
+    return a - b;
+}
+
+int multiply(int a, int b) 
+{
+    return a * b;
+}
+
+int divide(int a, int b) 
+
+{
+    if (b != 0)
+        return a / b;
+    else
+        return 0; // Return 0 for divide by zero
+}
