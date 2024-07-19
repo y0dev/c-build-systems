07db4ef082d6bc234515122acab5b05402350409
Devontae Reid <devdoesit17@gmail.com>
2024-07-18 23:26:06 -0500

    Initial Static Library

---
+++ windows/ar_example/sources/arithmetic.c
@@ -0,0 +1,25 @@
+#include "arithmetic.h"
+
+int add(int a, int b) 
+{
+    return a + b;
+}
+
+int subtract(int a, int b) 
+{
+    return a - b;
+}
+
+int multiply(int a, int b) 
+{
+    return a * b;
+}
+
+int divide(int a, int b) 
+
+{
+    if (b != 0)
+        return a / b;
+    else
+        return 0; // Return 0 for divide by zero
+}
